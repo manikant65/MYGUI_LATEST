709d8c8be6b344cd59101f7588d2645bac4dc598
Manikant Kumar <manikant38110298@gmail.com>
2025-07-14 21:45:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from gui import MainWindow
 from data_processor import DataProcessor
 
 def main():
+    mode = "file"  # Default to file mode
+    if len(sys.argv) > 1 and sys.argv[1] in ["console", "file"]:
+        mode = sys.argv[1]
+    
     data_queue = Queue()
-    processor = DataProcessor(data_queue)
+    processor = DataProcessor(data_queue, mode=mode)
     app = QApplication(sys.argv)
     window = MainWindow(data_queue, processor)
     window.show()
@@ -14,8 +18,10 @@ def main():
 
 if __name__ == "__main__":
     main()'''
-
-
+    
+    
+    
+    
     
 import sys
 from queue import Queue
@@ -29,7 +35,7 @@ def main():
         mode = sys.argv[1]
     
     data_queue = Queue()
-    processor = DataProcessor(data_queue, mode=mode)
+    processor = DataProcessor(data_queue, mode=mode, input_string="default_input")
     app = QApplication(sys.argv)
     window = MainWindow(data_queue, processor)
     window.show()
